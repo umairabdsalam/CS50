e6bc52617d281204358c58827a6444641eac47ca
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-03-17 23:26:14 +0000

    Added files from the conditionals and loops lectures

---
+++ hat.py
@@ -0,0 +1,10 @@
+import random
+class Hat:
+
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    @classmethod
+    def sort(cls, name):
+        house = random.choice(cls.houses)
+        print(name, "is in", house)
+
+Hat.sort("Harry")
